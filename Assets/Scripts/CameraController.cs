f885e49c09c47c89885d6bd2b1a8516578603ee3
Aaron <aaron.gedmin@gmail.com>
2015-05-16 16:42:30 -0400

    Added CameraControl.cs
    provides basic camera controls using WASD keys and mouse cursor

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+	public int camSpeed = 10;	// universal generic camera control speed value
+
+	// Update is called once per frame
+	void Update ()
+	{
+		float keyboardX = Input.GetAxis ("Horizontal");
+		float keyboardY = Input.GetAxis("Vertical");
+		float mouseX = Input.mousePosition.x;
+		float mouseY = Input.mousePosition.y;
+		int scrollDistance = 5;
+
+		transform.Translate (keyboardX * Time.deltaTime * camSpeed, 0, keyboardY * Time.deltaTime * camSpeed);	// WASD controls for keyboard movement of camera
+
+		// mouse scroll controls
+		// work in progress
+		if (mouseX < scrollDistance)
+		{
+			transform.Translate(-1, 0, 0);
+		}
+		if (mouseX >= Screen.width - scrollDistance)
+		{
+			transform.Translate (1, 0, 0);
+		}
+		if (mouseY < scrollDistance)
+		{
+			transform.Translate (0, 0, -1);
+		}
+		if (mouseY >= Screen.height - scrollDistance)
+		{
+			transform.Translate (0, 0, 1);
+		}
+	}
+}
