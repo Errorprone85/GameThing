83f5aaeeb7db88923f4b6f1e7ad0a9c71e944350
Errorprone85 <aaron.gedmin@gmail.com>
2015-05-17 22:12:34 -0400

    Changed code for button creation to be more friendly

--- Assets/Scripts/MenuSetup.cs
+++ Assets/Scripts/MenuSetup.cs
@@ -8,24 +8,32 @@ public class MenuSetup : MonoBehaviour
 	public static List<Texture2D> buildingUnitTextureRollover = new List<Texture2D>();
 	public Texture2D iconContainer;
 
+	private GUIStyle container = new GUIStyle ();
+
 	void OnGUI()
 	{
-		string[] buttonNames = { "?", "Barracks", "Resources", "Towers" };	// List of names to be used by the main buttons
-		GUIStyle container = new GUIStyle ();
-
 		container.normal.background = iconContainer;	// sets the default background of the container
 
 		// create main GUI box
 		GUI.Box (new Rect (Screen.width - 100, Screen.height - 200, 100, 200), "", container);
 
-		// set the initial position of the GUI buttons
-		int y = 80;
-
 		// creates GUI buttons
-		for (int i = 0; i < 4; i++)
+		// SlideMenu method takes the name of the button as an argument and creates a contextual menu based on that
+		if (GUI.Button (new Rect(Screen.width - 100, Screen.height - 80, 100, 30), "?"))
 		{
-			GUI.Button (new Rect (Screen.width - 100, Screen.height - y, 100, 30), buttonNames[i]);
-			y = y + 40;
+			print("?");
+		}
+		if (GUI.Button (new Rect(Screen.width - 100, Screen.height - 120, 100, 30), "Resources"))
+		{
+			print ("Resources");
+		}
+		if (GUI.Button (new Rect(Screen.width - 100, Screen.height - 160, 100, 30), "Barracks"))
+		{
+			print ("Barracks");
+		}
+		if (GUI.Button (new Rect(Screen.width - 100, Screen.height - 200, 100, 30), "Towers"))
+		{
+			 print ("Towers");
 		}
 	}
 }
